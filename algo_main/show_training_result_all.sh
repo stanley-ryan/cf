2ce60c0c5067c865fe59c71d8f30a7da074e94bd
stanley-ryan <stanley_81724@hotmail.com>
2018-07-07 22:59:44 +0800

    modify sh

--- algo_main/show_training_result_all.sh
+++ algo_main/show_training_result_all.sh
@@ -1,4 +1,4 @@
 python show_train_log.py --flog train_lenet_gpu/trainlog_lenet_fullweights_train.log --title train_lenet_full_weights
 python show_train_log.py --flog train_lenet_gpu/trainlog_lenet_prune60_train.log --title train_lenet_prune60_weights
 python show_train_log.py --flog train_lenet_gpu/trainlog_lenet_prune90_train.log --title train_lenet_prune90_weights
-python show_train_log.py --flog train_lenet_gpu_xnor/trainlog_lenet_xnor_train_weights.log --title train_lenet_conv_xnor
+python show_train_log.py --flog train_lenet_gpu_xnor/trainlog_lenet_xnor_train_weights.log --title train_lenet_conv_xor
